b4801fdd3fee24933bcc43f6fa0a4e2e8b8f1d8e
pedrothecatholic <pedro.sousa21@outlook.com>
2024-02-05 09:03:24 -0300

    First project commit && Dockerfile deploy

---
+++ src/main/java/com/pedrothecatholic/todolist/user/UserModel.java
@@ -0,0 +1,27 @@
+package com.pedrothecatholic.todolist.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_users")
+public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID ID;
+
+    @Column(unique = true)
+    public String username;
+    public String name;
+    public String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
